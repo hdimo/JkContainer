f9c20c4a83367539a3920f173c09fa6777f3cdd8
khaled <khaled.hadj.a@gmail.com>
2017-08-28 12:54:52 +0100

    add some tests

--- test/ContainerTest.php
+++ test/ContainerTest.php
@@ -4,20 +4,36 @@ namespace JkTest\Container;
 use PHPUnit\Framework\TestCase;
 
 use Jk\Container\Container;
-use Jk\Container\Exception\ServiceNotFoundException;
+
 
 class ContainerTest extends TestCase
 {
-
     private $service;
+    private $parameters;
+    private $container;
 
     public function setUp(){
-        $this->service = [];
+        $this->service = [
 
+        ];
+        $this->parameters = [
+            "password"=>"pass"
+        ];
+
+        $this->container = new Container($this->service,  $this->parameters);
     }
 
     /**
-     * @expectedException \Exception
+     * @expectedException Jk\Container\Exception\ContainerException
+     */
+    public function testCeateServiceContainerExceptionWithoutClassKey(){
+        $services = ["myObject"=>new \stdClass()];
+        $container = new Container($services, []);
+        $container->get("myObject");
+    }
+
+    /**
+     * @expectedException Jk\Container\Exception\ServiceNotFoundException
      */
     public function testServiceNotFoundException(){
         $service = [];
